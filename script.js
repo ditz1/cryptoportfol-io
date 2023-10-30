bd1edaa77ef6c4211ea6e87461f02c97fbc29e8c
dan <ditzcrypto@gmail.com>
2023-10-30 05:33:37 -0400

    changed name and added uid functions along with some other stuff

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-console.log("hello world");
+console.log("hello javascript");
+console.log("this language sucks");
 
-var showsignin = true;
+////////////////////////////////////////////////
+////////////////////_INIT_//////////////////////
+////////////////////////////////////////////////
+
 var signinpage = document.getElementById('loginscreen');
+var foliopage = document.getElementById('folioscreen');
+var uidtext = document.getElementById('loguid');
+var showsignin = true;
+var showfolio = false;
+var uid = 0;
+
+////////////////////////////////////////////////
+////////////////////SIGNIN//////////////////////
+////////////////////////////////////////////////
+
 console.log(showsignin);
-
-
-
-
-
 function loginCredentials(name, password) {
     fetch('http://35.188.225.12/backend/signin.php', {
         method: 'POST',
@@ -24,8 +33,12 @@ function loginCredentials(name, password) {
         console.log(data + "test");
         if (data.trim() === "User signed in successfully!"){
             showsignin = false;
-            
+            showfolio = true;
             signinpage.style.display = 'none';
+            foliopage.style.display = 'block';
+            getUserID(name);
+            
+            
         }
     })
     .catch(error => {
@@ -49,7 +62,8 @@ function createCredentials(name, password) {
         console.log(data + "test");
         if (data.trim() === "User signed in successfully!"){
             showsignin = false;
-            //signinpage.style.display = 'none';
+            showfolio = true;
+            
         }
     })
     .catch(error => {
@@ -57,34 +71,52 @@ function createCredentials(name, password) {
     });
 }
 
+function getUserID(name) {
+    fetch('http://35.188.225.12/backend/getuid.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'success') {
+            console.log("User found with UID:", data.data.uid);
+            uid = data.data.uid;
+            uidtext.textContent = "user id: " + uid;
+            loadFolioDisplay();
+        } else if (data.status === 'error') {
+            console.log("Error:", data.message);
+        }
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    });
+}
 
 document.getElementById('loginbutton')
         .addEventListener('click', function() {
             var username = document.getElementById('loginname');
             var password = document.getElementById('loginpass');
-           
             let slen = username.value.length;
             let plen = password.value.length;
+            
             if (slen < 3) {
                 console.log("username must be longer than 3 characters!");
             }
-            
             else if (plen < 3) {
                 console.log("password must be longer than 3 characters!");
             }
-            
             else {
                 console.log("created username: " + username.value);
                 console.log("created password: " + password.value);
                 loginCredentials(username.value, password.value);
                 username.value = "";
                 password.value = "";
-                //showsignin = false;
-                
                 console.log(showsignin);
-                //signinpage.style.display = 'none';
-
-
             }
         });
 
@@ -108,14 +140,22 @@ document.getElementById('createbutton')
                 console.log("created username: " + username.value);
                 console.log("created password: " + password.value);
                 createCredentials(username.value, password.value);
-                
                 username.value = "";
-                password.value = "";
-                //showsignin = false;
-                
+                password.value = "";                
                 console.log(showsignin);
-                //signinpage.style.display = 'none';
 
             }
         });
+
+////////////////////////////////////////////////
+////////////////////PORTFOLIO///////////////////
+////////////////////////////////////////////////
+function loadFolioDisplay(){
+    if (showfolio) {
+        console.log("folio showing");
+        console.log(uid);
+        console.log(uidtext.textContent);
+    }
+}
+
 
